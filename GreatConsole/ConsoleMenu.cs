5233ece44a8de41c75bf9ac84000d4edf3bac146
mineTomek <lukom.tomek@gmail.com>
2024-02-09 17:37:03 +0100

    Move MenuOption to a seperate file and share it for both menus

--- GreatConsole/ConsoleMenu.cs
+++ GreatConsole/ConsoleMenu.cs
@@ -78,18 +78,4 @@ public class ConsoleMenu
 
         return (selectedIndex, options[selectedIndex].name);
     }
-
-    public class MenuOption
-    {
-        public string name;
-        public AllowedColor standardColor;
-        public AllowedColor selectedColor;
-
-        public MenuOption(object _name, AllowedColor _standardColor = DarkYellow, AllowedColor _selectedColor = White)
-        {
-            name = _name.ToString()!;
-            standardColor = _standardColor;
-            selectedColor = _selectedColor;
-        }
-    }
 }
---
+++ GreatConsole/MenuOption.cs
@@ -0,0 +1,19 @@
+﻿using static GreatConsole.ConsoleColors.AllowedColor;
+using static GreatConsole.ConsoleColors;
+
+namespace GreatConsole;
+
+public class MenuOption
+{
+    public string name;
+    public AllowedColor standardColor;
+    public AllowedColor selectedColor;
+
+    public MenuOption(object _name, AllowedColor _standardColor = DarkYellow, AllowedColor _selectedColor = White)
+    {
+        name = _name.ToString()!;
+        standardColor = _standardColor;
+        selectedColor = _selectedColor;
+    }
+}
+
